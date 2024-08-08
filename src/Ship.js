4c4f2f6b41ccb334b7486623523e3b87e4b31128
Robert Marshall <Robert.Marshall@Boxfusion.io>
2024-08-08 14:11:40 +0200

    [feature] ship styling

--- src/Ship.js
+++ src/Ship.js
@@ -14,7 +14,7 @@ export default class Ship {
     this.rotationSpeed = 6;
     this.speed = 0.15;
     this.inertia = 0.99;
-    this.radius = 20;
+    this.radius = 40;// Increased radius for larger ship
     this.lastShot = 0;
     this.create = args.create;
     this.onDie = args.onDie;
@@ -131,43 +131,88 @@ export default class Ship {
       this.autoAim(state.astroidPostion);
     }
 
-    // Move
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    this.velocity.x *= this.inertia;
-    this.velocity.y *= this.inertia;
-
-    // Rotation
-    if (this.rotation >= 360) {
-      this.rotation -= 360;
-    }
-    if (this.rotation < 0) {
-      this.rotation += 360;
-    }
-
-    // Screen edges
-    if(this.position.x > state.screen.width) this.position.x = 0;
-    else if(this.position.x < 0) this.position.x = state.screen.width;
-    if(this.position.y > state.screen.height) this.position.y = 0;
-    else if(this.position.y < 0) this.position.y = state.screen.height;
-
-    // Draw
-    const context = state.context;
-    context.save();
-    context.translate(this.position.x, this.position.y);
-    context.rotate(this.rotation * Math.PI / 180);
-    context.strokeStyle = '#ffffff';
-    context.fillStyle = '#000000';
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(0, -15);
-    context.lineTo(10, 10);
-    context.lineTo(5, 7);
-    context.lineTo(-5, 7);
-    context.lineTo(-10, 10);
-    context.closePath();
-    context.fill();
-    context.stroke();
-    context.restore();
+     // Move
+     this.position.x += this.velocity.x;
+     this.position.y += this.velocity.y;
+     this.velocity.x *= this.inertia;
+     this.velocity.y *= this.inertia;
+  
+     // Rotation
+     if (this.rotation >= 360) {
+       this.rotation -= 360;
+     }
+     if (this.rotation < 0) {
+       this.rotation += 360;
+     }
+  
+     // Screen edges
+     if (this.position.x > state.screen.width) this.position.x = 0;
+     else if (this.position.x < 0) this.position.x = state.screen.width;
+     if (this.position.y > state.screen.height) this.position.y = 0;
+     else if (this.position.y < 0) this.position.y = state.screen.height;
+  
+     // Draw
+     const context = state.context;
+     context.save();
+     context.translate(this.position.x, this.position.y);
+     context.rotate(this.rotation * Math.PI / 180);
+  
+     // Draw Rocket Body
+     context.beginPath();
+     context.moveTo(0, -60); // Nose cone
+     context.lineTo(20, -20); // Right side of body
+     context.lineTo(20, 20);  // Right side of fins
+     context.lineTo(-20, 20); // Left side of fins
+     context.lineTo(-20, -20); // Left side of body
+     context.closePath();
+     context.fillStyle = '#f00'; // Red body
+     context.fill();
+     context.strokeStyle = 'black';
+     context.lineWidth = 4;
+     context.stroke();
+  
+     // Draw Rocket Nose Cone
+     context.beginPath();
+     context.moveTo(0, -60); // Nose tip
+     context.lineTo(20, -20); // Right side of body
+     context.lineTo(-20, -20); // Left side of body
+     context.closePath();
+     context.fillStyle = '#ff0'; // Yellow nose cone
+     context.fill();
+     context.stroke();
+  
+     // Draw Rocket Fins
+     context.beginPath();
+     context.moveTo(-20, 20); // Left base
+     context.lineTo(-30, 40); // Left fin
+     context.lineTo(-20, 30); // Base of fin
+     context.closePath();
+     context.fillStyle = '#00f'; // Blue fins
+     context.fill();
+     context.stroke();
+  
+     context.beginPath();
+     context.moveTo(20, 20); // Right base
+     context.lineTo(30, 40); // Right fin
+     context.lineTo(20, 30); // Base of fin
+     context.closePath();
+     context.fillStyle = '#00f'; // Blue fins
+     context.fill();
+     context.stroke();
+  
+     // Draw Thruster Flame if accelerating
+     if (state.keys.up) {
+       context.fillStyle = 'orange';
+       context.beginPath();
+       context.moveTo(-10, 20);
+       context.lineTo(0, 60);
+       context.lineTo(10, 20);
+       context.closePath();
+       context.fill();
+       context.strokeStyle = 'red';
+       context.stroke();
+     }
+  
+     context.restore();
   }
 }
